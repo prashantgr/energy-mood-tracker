dea672f528ec0c3e98f71e42a8a93da253ce0f63
Prashant G Rathod <prashant16gr@gmail.com>
2025-05-19 12:54:58 +0200

    Update sentiment-summary.service.ts

--- src/app/services/sentiment-summary.service.ts
+++ src/app/services/sentiment-summary.service.ts
@@ -23,7 +23,7 @@ export class SentimentSummaryService {
   private http = inject(HttpClient);
 
   // Replace with your Logic App or API endpoint URL
-  private summaryUrl = 'https://prod-82.westeurope.logic.azure.com:443/workflows/1f3643bd798c4af3b565c9366cc5fa9d/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=43UYqwI4XsAB4kmGdyL6v4lq6MnXy3Np_jSRY5FyzVU';
+  private summaryUrl = '';
 
   getSentimentSummary(): Observable<SentimentSummary[]> {
     return this.http.get<SentimentSummary[]>(this.summaryUrl);
